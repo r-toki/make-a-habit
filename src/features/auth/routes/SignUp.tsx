b1e0a3a9c21603894f97a0bac6a6004066efce34
rtokkie <natto.and.eggs@gmail.com>
2022-05-22 15:02:12 +0900

    refactor

--- src/features/auth/routes/SignUp.tsx
+++ src/features/auth/routes/SignUp.tsx
@@ -2,10 +2,9 @@ import { Button, Center, Container, Divider, Heading, Stack } from '@chakra-ui/r
 import { FC } from 'react';
 import { z } from 'zod';
 
-import { Form } from '../../../components/Form/Form';
-import { InputField } from '../../../components/Form/InputField';
-import { Link } from '../../../components/Link/Link';
-import { useSignUp } from '../api/useSignUp';
+import { Form, InputField } from '../../../components/Form';
+import { Link } from '../../../components/Link';
+import { useSignUp } from '../api';
 
 const schema = z
   .object({
