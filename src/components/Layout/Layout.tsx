a626084f541d7de7b4153c52a8099438fa2e982a
rtokkie <natto.and.eggs@gmail.com>
2022-05-22 22:05:29 +0900

    f

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
-import { Box, Center, Container, Flex, Heading, Stack, VStack } from '@chakra-ui/react';
+import { Box, Center, Container, Flex, Heading, Stack } from '@chakra-ui/react';
 import { FC, ReactNode } from 'react';
 import { BiHistory, BiMenu, BiNote } from 'react-icons/bi';
+import { useLocation } from 'react-router-dom';
 
 type LayoutProps = {
   title: string;
@@ -8,6 +9,9 @@ type LayoutProps = {
 };
 
 export const Layout: FC<LayoutProps> = ({ title, children }) => {
+  const location = useLocation();
+  const path = location.pathname.split('/').slice(1).join('/');
+
   return (
     <Stack h="full">
       <Box py="4" borderBottomWidth="1px">
@@ -25,21 +29,33 @@ export const Layout: FC<LayoutProps> = ({ title, children }) => {
       <Box py="2" borderTopWidth="1px" bg="gray.50">
         <Container>
           <Flex justifyContent="space-around">
-            <Flex direction="column" alignItems="center">
+            <Flex
+              direction="column"
+              alignItems="center"
+              color={
+                path.startsWith('app/habits') && !path.startsWith('app/habits/all')
+                  ? 'black'
+                  : 'gray.500'
+              }
+            >
               <BiNote fontSize="30px" />
               <Box fontWeight="bold" fontSize="xs">
                 Record
               </Box>
             </Flex>
 
-            <Flex direction="column" alignItems="center">
+            <Flex
+              direction="column"
+              alignItems="center"
+              color={path.startsWith('app/habits/all') ? 'black' : 'gray.500'}
+            >
               <BiHistory fontSize="30px" />
               <Box fontWeight="bold" fontSize="xs">
                 History
               </Box>
             </Flex>
 
-            <Flex direction="column" alignItems="center">
+            <Flex direction="column" alignItems="center" color="gray.500">
               <BiMenu fontSize="30px" />
               <Box fontWeight="bold" fontSize="xs">
                 Menu
