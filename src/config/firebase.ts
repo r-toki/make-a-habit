3d686eb6596ce6b4af8fe06e473c5a976d3a59b6
rtokkie <natto.and.eggs@gmail.com>
2022-05-22 12:21:53 +0900

    add initializeFirebaseApp

---
+++ src/config/firebase.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+
+const config = {
+  apiKey: 'AIzaSyDPNRZICBrLoUOCNEnsCh4k_nRxKOkWpT0',
+  authDomain: 'make-a-habit-fd3f5.firebaseapp.com',
+  projectId: 'make-a-habit-fd3f5',
+  storageBucket: 'make-a-habit-fd3f5.appspot.com',
+  messagingSenderId: '165578706095',
+  appId: '1:165578706095:web:a95f32bc1d82d9c852585a',
+  measurementId: 'G-8DEQ32LR47',
+};
+
+export function initializeFirebaseApp() {
+  initializeApp(config);
+
+  if (!import.meta.env.PROD) {
+    connectAuthEmulator(getAuth(), 'http://localhost:9099', { disableWarnings: true });
+    connectFirestoreEmulator(getFirestore(), 'localhost', 8080);
+  }
+}
