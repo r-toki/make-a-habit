348dfd01f737602738e81675e81727192087d120
rtokkie <natto.and.eggs@gmail.com>
2022-05-22 21:59:47 +0900

    add Habits

---
+++ src/features/habits/routes/Habits.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react';
+import { FC } from 'react';
+
+import { Layout } from '@/components/Layout';
+
+export const Habits: FC = () => {
+  return (
+    <Layout title="Habits">
+      <Box py="4"></Box>
+    </Layout>
+  );
+};
--- src/features/habits/routes/index.tsx
+++ src/features/habits/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { NewHabit } from './new';
+import { Habits } from './Habits';
+import { NewHabit } from './NewHabit';
 
 export const HabitsRoutes = () => {
   return (
     <Routes>
+      <Route path="" element={<Habits />} />
       <Route path="new" element={<NewHabit />} />
     </Routes>
   );
