e472025302a4081880068c323de8bd63bb63f68d
rtokkie <natto.and.eggs@gmail.com>
2022-05-22 13:57:46 +0900

    feat sign up

--- src/features/auth/routes/SignUp.tsx
+++ src/features/auth/routes/SignUp.tsx
@@ -1,5 +1,92 @@
+import { Button, Center, Container, Divider, Heading, Stack } from '@chakra-ui/react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { FC } from 'react';
+import { z } from 'zod';
+
+import { Form } from '../../../components/Form/Form';
+import { InputField } from '../../../components/Form/InputField';
+import { Link } from '../../../components/Link/Link';
+
+const schema = z
+  .object({
+    email: z.string().min(1, 'Required'),
+    password: z.string().min(1, 'Required'),
+    confirm: z.string().min(1, 'Required'),
+    name: z.string().min(1, 'Required'),
+  })
+  .refine((fields) => fields.password === fields.confirm, {
+    message: "Passwords don't match",
+    path: ['confirm'],
+  });
+
+type RegisterValues = {
+  email: string;
+  password: string;
+  confirm: string;
+  name: string;
+};
 
 export const SignUp: FC = () => {
-  return <div>Sing Up</div>;
+  const signUp = async ({ email, password }: { email: string; password: string }) => {
+    // await signInWithEmailAndPassword(getAuth(), email, password);
+  };
+
+  return (
+    <Container maxW="md" py="4">
+      <Stack>
+        <Center>
+          <Heading>Make a Habit!</Heading>
+        </Center>
+        <Form<RegisterValues, typeof schema>
+          onSubmit={({ email, password }) => {
+            signUp({ email, password });
+          }}
+          schema={schema}
+        >
+          {({ register, formState }) => (
+            <Stack spacing="4">
+              <Stack>
+                <InputField
+                  label="name"
+                  registration={register('name')}
+                  error={formState.errors.name}
+                />
+
+                <InputField
+                  label="email"
+                  registration={register('email')}
+                  error={formState.errors.email}
+                />
+
+                <InputField
+                  type="password"
+                  label="password"
+                  registration={register('password')}
+                  error={formState.errors.password}
+                  autoComplete="on"
+                />
+
+                <InputField
+                  type="password"
+                  label="confirm"
+                  registration={register('confirm')}
+                  error={formState.errors.confirm}
+                  autoComplete="on"
+                />
+              </Stack>
+
+              <Divider />
+
+              <Button type="submit" colorScheme="green">
+                Sign Up
+              </Button>
+            </Stack>
+          )}
+        </Form>
+        <Link alignSelf="start" to="/auth/log-in">
+          to Log In
+        </Link>
+      </Stack>
+    </Container>
+  );
 };
