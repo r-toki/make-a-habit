88020010046da2af661ac1b0cb187b8f2de3923f
rtokkie <natto.and.eggs@gmail.com>
2022-05-22 12:34:19 +0900

    add chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -8,6 +9,8 @@ initializeFirebaseApp();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
