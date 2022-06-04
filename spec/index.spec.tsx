d20fb3e8ed26dd025729e9ed1e52c0ae8d856d92
rtokkie <natto.and.eggs@gmail.com>
2022-06-04 11:34:53 +0900

    d

--- spec/index.spec.tsx
+++ spec/index.spec.tsx
@@ -1,14 +1,19 @@
-import { initializeTestEnvironment } from '@firebase/rules-unit-testing';
-import { render, screen } from '@testing-library/react';
+import { initializeTestEnvironment, RulesTestContext } from '@firebase/rules-unit-testing';
+import { fireEvent, render } from '@testing-library/react';
 import { collection, doc, getDoc, Timestamp } from 'firebase/firestore';
 import fs from 'fs';
+import { useState } from 'react';
+import { afterAll, beforeAll, expect, it } from 'vitest';
 
 import { UsersCollection } from '@/fire/collections';
 import { UserDoc } from '@/fire/docs';
 
-// TODO:
-// 1. render async component and test connect firestore
-// 2. di collection for test ( to create context of root collections )
+// NOTE: refs
+//       1. setup -> https://www.eternaldev.com/blog/testing-a-react-application-with-vitest/
+//       2. how to use getBy, queryBy and findBy -> https://qiita.com/nazeudon/items/28731673dad537c06d72
+
+// UTIL:
+const ts = (iso: string) => Timestamp.fromDate(new Date(iso));
 
 const testEnv = await initializeTestEnvironment({
   projectId: 'make-a-habit-fd3f5',
@@ -19,55 +24,60 @@ const testEnv = await initializeTestEnvironment({
   },
 });
 
-const userId = '1';
+const sudo = (cb: ({ db }: { db: ReturnType<RulesTestContext['firestore']> }) => Promise<void>) => {
+  return testEnv.withSecurityRulesDisabled((ctx) =>
+    cb({
+      db: ctx.firestore(),
+    })
+  );
+};
 
-const authCtx = testEnv.authenticatedContext(userId);
-const clientDb = authCtx.firestore();
+const authCtx = testEnv.authenticatedContext('0');
+const authDb = authCtx.firestore();
 
-const usersCollection = new UsersCollection(collection(clientDb, 'users'));
+const usersCollection = new UsersCollection(collection(authDb, 'users'));
 
 beforeAll(async () => {
-  testEnv.clearFirestore();
+  await testEnv.clearFirestore();
 });
 
 afterAll(async () => {
-  testEnv.clearFirestore();
+  await testEnv.clearFirestore();
+  await testEnv.clearFirestore();
 });
 
-it('trivial', async () => {
-  const user = new UserDoc({
-    id: userId,
-    ref: doc(usersCollection.ref, userId),
-    data: () => ({
-      name: 'EIZO',
-      createdAt: Timestamp.fromDate(new Date('2000-01-01')),
-      updatedAt: Timestamp.fromDate(new Date('2000-01-01')),
-    }),
-  });
+it('create user', async () => {
+  const user = UserDoc.create(usersCollection, '0', { name: 'EIZO' });
   await user.save();
 
-  await testEnv.withSecurityRulesDisabled(async (ctx) => {
-    const db = ctx.firestore();
-    const gotUser = await getDoc(doc(db, 'users', '1'));
+  await sudo(async ({ db }) => {
+    const gotUser = await getDoc(doc(db, 'users', '0'));
 
-    expect(gotUser.data()).toStrictEqual({
-      name: 'EIZO',
-      createdAt: Timestamp.fromDate(new Date('2000-01-01')),
-      updatedAt: Timestamp.fromDate(new Date('2000-01-01')),
-    });
+    expect(gotUser.data()?.name).toBe('EIZO');
   });
 });
 
-// const App = () => {
-//   return <div>TITLE</div>;
-// };
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => setTimeout(() => setCount((v) => v + 1), 100);
+  const decrement = () => setTimeout(() => setCount((v) => v - 1), 200);
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={increment}>plus 1</button>
+      <button onClick={decrement}>minus 1</button>
+    </div>
+  );
+};
 
-// it('component', async () => {
-//   render(<App />);
+it('async count component', async () => {
+  const { findByText, getByText } = render(<Counter />);
 
-//   expect(screen.getByText('TITLE')).toBeInTheDocument();
-// });
+  expect(await findByText('0')).toBeDefined();
 
-const HelloWorld = () => <h1>Hello World</h1>;
-const { debug } = render(<HelloWorld />);
-debug();
+  fireEvent.click(getByText('plus 1'));
+  expect(await findByText('1')).toBeDefined();
+
+  fireEvent.click(getByText('minus 1'));
+  expect(await findByText('0')).toBeDefined();
+});
